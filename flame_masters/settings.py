12cb73c5c938f9404cf80ff4232cf5f29f8829ca
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-01-21 20:14:48 +0000

    Added home app and templates for the homepage

--- flame_masters/settings.py
+++ flame_masters/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,10 @@ ROOT_URLCONF = 'flame_masters.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
