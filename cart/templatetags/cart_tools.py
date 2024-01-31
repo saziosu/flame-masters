05f1b7caef6dfec71eb0f9361dcf5c16fc46f31d
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-01-31 18:09:40 +0000

    Add cart_tools.py to calculate subtotals in cart

---
+++ cart/templatetags/cart_tools.py
@@ -0,0 +1,9 @@
+from django import template
+
+
+register = template.Library()
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    # calculates the subtotal for each item
+    return price * quantity
