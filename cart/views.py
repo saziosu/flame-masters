1ff3768a26b38d73d53e5546c90dda9d3d4720a0
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-01-31 18:02:55 +0000

    Added modify_cart and remove_from_cart views

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_cart(request):
@@ -18,9 +18,43 @@ def add_to_cart(request, item_id):
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
+        # if this item is already in the cart, add to it
         cart[item_id] += quantity
     else:
+        # else, add the quantity
         cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def modify_cart(request, item_id):
+    """
+    View to handle modifying the quantity of an item in the cart
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """
+    View to remove items from the cart
+    """
+
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
