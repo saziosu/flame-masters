cf557bed7b598b1ad56559d63265760deec04779
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-05 16:14:38 +0000

    Added Stripe card element to the checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OPq0ACAmUbTReN0tow1hSsD6v6cERV8ZKezOt38l1SVZS1wAHvGlE8mb9hIoZ4MBSM3htwzuylLjLt3W8wVeIpb007BF3DhUo',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
