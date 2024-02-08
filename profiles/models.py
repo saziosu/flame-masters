39de500e5ce50b1a920f8d3771a804d9442732c0
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-08 18:04:17 +0000

    Add user profile, updated the views, templates and css for profiles

--- profiles/models.py
+++ profiles/models.py
@@ -35,5 +35,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
-    # Existing users: just save the profile
+    # If the user profile exists save it
     instance.userprofile.save()
