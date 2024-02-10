0ac7a0e8cecd63826b6576d593d564fe4de6f63e
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-10 15:24:28 +0000

    Added products/forms.py to allow for front end product admin activity

---
+++ products/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Product, Category, Brand, HeatLevel
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        # include all fields from the Product model
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Set up categories as a choice in form
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        self.fields['category'].choices = friendly_names
+        # Set up brands as a choice in form
+        brands = Brand.objects.all()
+        friendly_names = [(b.id, b.get_friendly_name()) for b in categories]
+        self.fields['brand'].choices = friendly_names
+        # Set up heat_level as a choice in form
+        heat_levels = HeatLevel.objects.all()
+        friendly_names = [(h.id, h.get_friendly_name()) for h in categories]
+        self.fields['brand'].choices = friendly_names
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
