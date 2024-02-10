4395e2ee2a387c11181b28972f9e9bc4da4d76d4
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-10 18:19:41 +0000

    Add delete product url and view to the products app

--- products/views.py
+++ products/views.py
@@ -106,9 +106,9 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             # if the form is valid, save and send success message
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -146,3 +146,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    A view to handle deleting products
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
