8e0558d30a907ebc80635791fbd05cd55277452c
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-12 21:00:42 +0000

    Added staff_favourite booleanfield to product model and product detail template

--- products/models.py
+++ products/models.py
@@ -61,6 +61,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254, blank=False, null=False)
     description = models.TextField(null=False, blank=False)
     ingredients = models.TextField(null=False, blank=False)
+    staff_favourite = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
