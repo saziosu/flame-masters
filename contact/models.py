258cf6880c8baebd27b25273448b3ee7c4c98a85
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-13 20:24:02 +0000

    Add Contact Model for collecting contact form submissions

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
+    """
+    A model to store contact form entries
+    """
     Q_CHOICES = (
         ('general_query', 'General Query'),
         ('return', 'Return'),
@@ -9,11 +12,17 @@ class Contact(models.Model):
     )
 
     # The type of query used
-    q_type = models.CharField(max_length=14, choices=Q_CHOICES)
+    reason = models.CharField(max_length=14, choices=Q_CHOICES)
     order_number = models.CharField(max_length=50, null=True, blank=True)
     name = models.CharField(max_length=30, null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
-    message = models.TextField(max_length=3000, null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # order the contact form posts on when they were created
+        # newest first
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.email
