28a99fbc19a22141488e8e4eed8c30de90ebfd47
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-13 20:23:04 +0000

    Contact form error, readd products after database reset

--- flame_masters/urls.py
+++ flame_masters/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('contact/', include('contact.urls')),
     # setting the static so django can see the media files
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
