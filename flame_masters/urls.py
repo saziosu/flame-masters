c93aa5db98035eb6e309454266bb5f0728bd09b2
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-15 18:22:48 +0000

    Update 500 error handler due to argument issue

--- flame_masters/urls.py
+++ flame_masters/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404, handler403
+from .views import handler404, handler403, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,6 +32,6 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'flame_masters.views.handler404'
-handler403 = 'flame_masters.views.handler403'
-handler500 = 'flame_masters.views.handler403'
+# handler403 = 'flame_masters.views.handler403'
+# handler500 = 'flame_masters.views.handler403'
 
--- flame_masters/views.py
+++ flame_masters/views.py
@@ -10,6 +10,6 @@ def handler403(request, exception):
     return render(request, "errors/403.html", status=403)
 
 
-def handler500(request, exception):
+def handler500(request):
     """ Error Handler 500 - Internal Server Error """
     return render(request, "errors/500.html", status=500)
