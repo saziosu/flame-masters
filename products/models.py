ccb91bcdfd5ee201dc1b750eb8513b65167f0be8
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-17 20:56:02 +0000

    Added the ProductReview model to collect user product reviews

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Category(models.Model):
     """
@@ -60,12 +60,26 @@ class Product(models.Model):
     heat_level = models.ForeignKey('HeatLevel', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254, blank=False, null=False)
     description = models.TextField(null=False, blank=False)
+    average_rating = models.FloatField(default=0)
     ingredients = models.TextField(null=False, blank=False)
     staff_favourite = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class ProductReview(models.Model):
+    """
+    A model to handle allowing reviews on products
+    https://www.youtube.com/watch?v=8iCqlFyFu2s
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_reviews')
+    rating = models.IntegerField()
+    comment = models.TextField()
+    reviewer = models.ForeignKey(User, related_name='product_reviews', on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    
