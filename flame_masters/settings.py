9aecec6eb3c96a9c4d54ffbb3d10b841be43b7a7
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-19 20:41:34 +0000

    Update settings.py to improve the user emails

--- flame_masters/settings.py
+++ flame_masters/settings.py
@@ -229,7 +229,7 @@ STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 # Email settings
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'flamemasters@example.com'
+    DEFAULT_FROM_EMAIL = 'flamemasters@flamemasters.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
