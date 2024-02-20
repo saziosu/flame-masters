19ec95ca216abce3195457b25f0803e7541c6e41
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-02-20 10:51:16 +0000

    Add url and template for editing products

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('review/<int:product_id>/', views.review_product, name='review_product'),
+    path('edit-review/<int:review_id>/', views.edit_product_review, name='edit_review'),
 ]
